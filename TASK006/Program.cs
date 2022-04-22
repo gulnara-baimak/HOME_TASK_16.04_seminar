2e60145fb909da73ccc9d36312b4843b337920c0
Gulnara <gulnara@mail.rum>
2022-04-22 22:46:44 +0600

    TASK006

---
+++ TASK006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// Test 4 -> да? -3 -> нет, 7 -> нет
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+if (a % 2 == 0)
+{
+    Console.WriteLine("ДA");
+}
+    if (a % 2 != 0)
+    {
+       Console.WriteLine("НЕТ"); 
+    }
+ 
